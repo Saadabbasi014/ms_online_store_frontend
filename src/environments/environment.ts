25ca5f0a56700474a4f29df4ebdf2039f42bda86
saad_abbasi <saad.abassi@orgonize.com>
2025-07-24 19:58:54 +0500

    remove recret key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,5 @@ export const environment = {
 
     production: false,
     baseUrl: '/api/',
-    Publishablekey: "pk_test_51RnLrOFL2l4x7PB19u9wDlakqs0Czn2yHt5WoEzqD5trExJTR2qXLGDBIAWVnwRW0v2iOiIaU9T8QYGlfnpIJaT000an6FYqBG",
-    Secretkey: "sk_test_51RnLrOFL2l4x7PB1z9lVWn6D1OZywwuhSIdaSAOAp1V59Tuyy0yVLmRKO6T5hkx6C7GEJtsNdZTjX5Sjmcu1i40X00MLrXAINP"
-
+    Publishablekey: "pk_test_51RnLrOFL2l4x7PB19u9wDlakqs0Czn2yHt5WoEzqD5trExJTR2qXLGDBIAWVnwRW0v2iOiIaU9T8QYGlfnpIJaT000an6FYqBG"
 };
